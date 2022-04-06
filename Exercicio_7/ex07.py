ca5ad6bf8f5d7c233a449e850f306d553d0336ac
oderlan <oderlanfreire@gmail.com>
2022-04-06 13:43:43 -0300

    Exercício 7 concluído

---
+++ Exercicio_7/ex07.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Insira a primeria nota do aluno: "))
+nota2 = float(input("Insira a segunda nota do aluno: "))
+nota3 = float(input("Insira a terceira nota do aluno: "))
+
+ma = (nota1+nota2+nota3)/3
+
+print('A media entre {}, {}, {} é {:.1f}' . format(nota1, nota2, nota3, ma))
