3ab1764357735608aaa56339fea1c31ef85c30b9
oderlan <oderlanfreire@gmail.com>
2022-04-13 14:10:32 -0300

    Exercício 12 concluído

---
+++ Exercicio_12/ex12.py
@@ -0,0 +1,8 @@
+produto = float(input("Insira o valor do produto: R$"))
+promo = float(
+    input("Qual a porcentagem de desconto?(Insira apenas o valor): "))
+
+promocao = produto - (produto * (promo/100))
+
+print(
+    'O produto na promoção com 5% de desconto sai por R${:.2f}' .format(promocao))
