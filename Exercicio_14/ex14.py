ae532a862bbad41d23d28eb53ea0f0a578fd1d07
oderlan <oderlanfreire@gmail.com>
2022-04-14 11:38:51 -0300

    Exercício 14 concluído

---
+++ Exercicio_14/ex14.py
@@ -0,0 +1,7 @@
+celsius = int(input("Insira a temperatura em Celsius °C: "))
+
+fahrenheit = (celsius*9/5)+32
+
+kelvin = celsius + 273.15
+
+print("A temperatura de {}°C em Fahrenheit e em Kelvin são respectivamente: {}°f, {}K" .format(celsius, fahrenheit, kelvin))
